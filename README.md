b6272c176448547c93155bc631341a4e4c54d1ce
kevin <kevin.vandenbreemen@gmail.com>
2023-11-09 15:41:58 -0800

    Remove the executable creation feature as this is more just a library

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
 # grucd
 
-## Requirements
-In order to use this library GraphViz should be installed on the system that will be running it
-
-## Installing
-To generate an executable version of the tool for now you can clone this project and run:
-
-```
-./gradlew FatJar
-```
-
-You can then run the tool with 
-java -jar grucd.jar
+A library that generates an abstraction of a codebase that you can then use to create artifacts such as UML etc.
 
 ## Contributing
 
-### Pushing changes and Updating the Version
-
-You can use the ```scripts/tagAndPush.sh``` script to tag a snapshot and push it as a new release.
--- build.gradle.kts
+++ build.gradle.kts
@@ -40,28 +40,6 @@ dependencies {
 
 }
 
-val fatJar = task("FatJar", type = Jar::class) {
-
-    val jarName = "grucd.jar"
-
-    archiveFileName.set(jarName)
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-    manifest {
-        attributes["Main-Class"] = "com.vandenbreemen.grucd.main.Main"
-
-    }
-    from(configurations.runtimeClasspath.get().map {
-        if(it.isDirectory) it else zipTree(it)
-    })
-    with(tasks.jar.get() as CopySpec)
-
-    copy {
-        from("build/libs/$jarName")
-        into("./")
-    }
-    println("Built and copied $jarName")
-}
-
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
